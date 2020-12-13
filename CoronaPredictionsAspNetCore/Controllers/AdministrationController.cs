d3f8a2dbaea0346545d26399582cdb07ad808f10
CoronaPredictionsAspNetCore <koutsoukosk@hotmail.com>
2020-12-13 03:50:35 +0200

    until client validation

--- CoronaPredictionsAspNetCore/Controllers/AdministrationController.cs
+++ CoronaPredictionsAspNetCore/Controllers/AdministrationController.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using CoronaPredictionsAspNetCore.Areas.Identity.Data;
 using CoronaPredictionsAspNetCore.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoronaPredictionsAspNetCore.Controllers
 {
+    [Authorize]
     public class AdministrationController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
--- CoronaPredictionsAspNetCore/Startup.cs
+++ CoronaPredictionsAspNetCore/Startup.cs
@@ -46,11 +46,12 @@ namespace CoronaPredictionsAspNetCore
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
+                app.UseStatusCodePagesWithReExecute("/Error/{0}");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+               app.UseHsts();
             }
-            app.UseHttpsRedirection();
+           // app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
